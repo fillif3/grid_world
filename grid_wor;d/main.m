77c606873f99a18764e15d936b0e73c66111a555
filip <filip@DESKTOP-8TEP3IM>
2021-10-18 21:17:49 +0200

    markov solution is now stohastic

--- grid_wor;d/main.m
+++ grid_wor;d/main.m
@@ -1,9 +1,9 @@
 %clear; clc
 hold on
-size_of_grid=50;
+size_of_grid=20;
 number_of_targets=10;
-numbaer_of_obstacles=50;
-type_of_solution = 'fuzzy';
+numbaer_of_obstacles=10;
+type_of_solution = 'markov';
 
 
 
@@ -14,7 +14,7 @@ if strcmp(type_of_solution,'fuzzy')
     fuzzy_solution(robot,targets,fire,number_of_targets,size_of_grid,1)
 end
 if strcmp(type_of_solution,'markov')
-    var=markov_solution(robot,targets,fire,number_of_targets,size_of_grid);
+    var=markov_solution(robot,targets,fire,number_of_targets,size_of_grid,0.8);
 end
 
 plot(targets(:,1),targets(:,2),'g*')
--- grid_wor;d/policy_global.m
+++ grid_wor;d/policy_global.m
@@ -1,15 +1,29 @@
 function [action] = policy_global(currentPosition,value_function)
-    size=length(value_function);
-    current_score=0;
-    action = [];
-    for i=[-1,0,1]
-        for j = [-1,0,1]
-            score = value_function(max(min(size,currentPosition(1)+i),1),max(min(size,currentPosition(2)+j),1));
-            if score>current_score
-                action=[i,j];
-                current_score=score;
+    if ismatrix(value_function)
+        size=length(value_function);
+        current_score=-inf;
+        action = [];
+        for i=[-1,0,1]
+            for j = [-1,0,1]
+                score = value_function(max(min(size,currentPosition(1)+i),1),max(min(size,currentPosition(2)+j),1));
+                if score>current_score
+                    action=[i,j];
+                    current_score=score;
+                end
+    
             end
-
         end
+    elseif ndims(value_function)==3
+        possible_actions = [-1,-1;
+        -1, 0;
+        -1, 1;
+        0, 1;
+        1, 1;
+        1, 0;
+        1, -1;
+        0, -1];
+        [val,idx] = max(value_function(currentPosition(1),currentPosition(2),:));
+        action = possible_actions(idx,:);
     end
+
 end
